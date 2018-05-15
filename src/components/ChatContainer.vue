7e03a9463676cbe43a6543eba7758abb0ddcd0f3
Amr Essam <amr.essam@emirates.com>
2018-05-16 00:56:46 +0400

    Added placeholder team

--- src/components/ChatContainer.vue
+++ src/components/ChatContainer.vue
@@ -2,14 +2,8 @@
   <v-container fluid>
     <v-navigation-drawer v-model="teamBar" app>
       <v-list>
-        <v-list-tile
-        v-for="item in menuItems"
-        :key="item.title"
-        :to="item.path">
-        <v-list-tile-action>
-          <v-icon>{{item.icon}}</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content >{{item.title}}</v-list-tile-content>
+        <v-list-tile>
+        <v-list-tile-content >Next Test Team</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
